f140c2c2bafecea6b28645477bcb7c29a92107bf
chandra sagar singuluri <sagarsinguluri2005@gmail.com>
2025-04-12 18:16:21 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Smooth scroll for navigation links
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
